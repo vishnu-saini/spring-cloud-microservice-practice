7803ef8be6e81d560e805bb1e4f233df81ff1b49
Vishnu Saini <vishnu.saini@publicissapient.com>
2021-02-25 17:08:24 +0530

    local changes

--- api-gateway/src/main/java/com/in28minutes/microservices/apigateway/LoggingFilter.java
+++ api-gateway/src/main/java/com/in28minutes/microservices/apigateway/LoggingFilter.java
@@ -13,12 +13,9 @@ import reactor.core.publisher.Mono;
 public class LoggingFilter implements GlobalFilter {
 
 	private Logger logger = LoggerFactory.getLogger(LoggingFilter.class);
-	
-	@Override
-	public Mono<Void> filter(ServerWebExchange exchange, 
-			GatewayFilterChain chain) {
-		logger.info("Path of the request received -> {}", 
-				exchange.getRequest().getPath());
+
+	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+		logger.info("Path of the request received -> {}", exchange.getRequest().getPath());
 		return chain.filter(exchange);
 	}
 
--- currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -12,51 +12,41 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CurrencyConversionController {
-	
+
 	@Autowired
 	private CurrencyExchangeProxy proxy;
-	
+
 	@GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
-	public CurrencyConversion calculateCurrencyConversion(
-			@PathVariable String from,
-			@PathVariable String to,
-			@PathVariable BigDecimal quantity
-			) {
-		
-		HashMap<String, String> uriVariables = new HashMap<>();
-		uriVariables.put("from",from);
-		uriVariables.put("to",to);
-		
-		ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate().getForEntity
-		("http://localhost:8000/currency-exchange/from/{from}/to/{to}", 
-				CurrencyConversion.class, uriVariables);
-		
+	public CurrencyConversion calculateCurrencyConversion(@PathVariable String from, @PathVariable String to,
+			@PathVariable BigDecimal quantity) {
+
+		HashMap<String, String> uriVariables = new HashMap();
+		uriVariables.put("from", from);
+		uriVariables.put("to", to);
+
+		ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate().getForEntity(
+				"http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConversion.class, uriVariables);
+
 		CurrencyConversion currencyConversion = responseEntity.getBody();
-		
-		return new CurrencyConversion(currencyConversion.getId(), 
-				from, to, quantity, 
-				currencyConversion.getConversionMultiple(), 
-				quantity.multiply(currencyConversion.getConversionMultiple()), 
-				currencyConversion.getEnvironment()+ " " + "rest template");
-		
+
+		return new CurrencyConversion(currencyConversion.getId(), from, to, quantity,
+				currencyConversion.getConversionMultiple(),
+				quantity.multiply(currencyConversion.getConversionMultiple()),
+				currencyConversion.getEnvironment() + " " + "rest template");
+
 	}
 
 	@GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
-	public CurrencyConversion calculateCurrencyConversionFeign(
-			@PathVariable String from,
-			@PathVariable String to,
-			@PathVariable BigDecimal quantity
-			) {
-				
-		CurrencyConversion currencyConversion = proxy.retrieveExchangeValue(from, to);
-		
-		return new CurrencyConversion(currencyConversion.getId(), 
-				from, to, quantity, 
-				currencyConversion.getConversionMultiple(), 
-				quantity.multiply(currencyConversion.getConversionMultiple()), 
-				currencyConversion.getEnvironment() + " " + "feign");
-		
-	}
+	public CurrencyConversion calculateCurrencyConversionFeign(@PathVariable String from, @PathVariable String to,
+			@PathVariable BigDecimal quantity) {
 
+		CurrencyConversion currencyConversion = proxy.retrieveExchangeValue(from, to);
+
+		return new CurrencyConversion(currencyConversion.getId(), from, to, quantity,
+				currencyConversion.getConversionMultiple(),
+				quantity.multiply(currencyConversion.getConversionMultiple()),
+				currencyConversion.getEnvironment() + " " + "feign");
+
+	}
 
 }
